023f4d273c08b777cb36748e3ec22f6fd915fac7
Beatriz Bravo <beatrizbravo538@gmail.com>
2025-10-18 07:50:52 -0500

    Initial commit

---
+++ app/src/main/java/edu/uesan/practica1moviles22200118/MainActivity.kt
@@ -0,0 +1,47 @@
+package edu.uesan.practica1moviles22200118
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import edu.uesan.practica1moviles22200118.ui.theme.PRACTICA1MOVILES22200118Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            PRACTICA1MOVILES22200118Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    PRACTICA1MOVILES22200118Theme {
+        Greeting("Android")
+    }
+}
